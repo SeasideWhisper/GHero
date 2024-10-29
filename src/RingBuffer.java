42d8a871b82302013f9f2c709d5ebdf04f015273
Lioncat6 <benjaminhouseholder0@gmail.com>
2024-10-29 13:19:30 -0500

    Added methods to ringbuffer

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -1,4 +1,40 @@
 
 public class RingBuffer {
-
+	
+	private double[] buffer;
+	private int first;
+	private int last;
+	public RingBuffer(int capacity) {
+		buffer = new double[capacity];
+		first = 0;
+		last = 0;
+	}
+	
+	public int size() {
+		return last-first;
+	}
+	
+	public boolean isEmpty() {
+		return (size() > 0);
+	}
+	
+	public boolean isFull() {
+		return (size() >= buffer.length);
+	}
+	
+	public void enqueue() {
+		
+	}
+	
+	public double dequeue() {
+		return 0;
+	}
+	
+	public double peek() {
+		return 0;
+	}
+	
+	public String toString() {
+		return null;
+	}
 }
