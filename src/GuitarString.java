a50a254a1ae31ac6b24ac491dae913a6bf3ba356
Arnav Jain <154857811+Dark-Knight-180@users.noreply.github.com>
2024-10-29 13:18:39 -0500

    initial push cause

---
+++ src/GuitarString.java
@@ -0,0 +1,25 @@
+public class GuitarString {
+
+    public ringBuffer;
+
+public GuitarString(double frequency){
+
+}
+public GuitarString(double[] init){
+
+}
+public void pluck(){
+
+}
+void tic(){
+
+}
+double sample(){
+
+}
+int time(){
+
+}
+
+    
+}
