3b147bad94d406907f51fd86d4ff2e8e75c9679f
SeasideWhisperer <benjaminhouseholder0@gmail.com>
2024-10-31 13:42:20 -0500

    we do be takin dem L's

--- src/GuitarString.java
+++ src/GuitarString.java
@@ -1,30 +1,45 @@
 
 public class GuitarString {
 
-    public ringBuffer;
-
+    public RingBuffer buffer;
+    private int capacity;
+    private int time;
     public GuitarString(double frequency) {
-
-        RingBuffer thigny = new RingBuffer(frequency);
+    	capacity = (int) (44100 / frequency);
+        RingBuffer buffer = new RingBuffer(capacity);
+        for (int x = 0; x< capacity; x++) {
+        	buffer.enqueue(0.0);
+        }
+        time = 0;
     }
 
     public GuitarString(double[] init) {
-
+    	capacity = init.length;
+        RingBuffer buffer = new RingBuffer(capacity);
+        for (int x = 0; x< capacity; x++) {
+        	buffer.enqueue(init[x]);
+        }
+        time = 0;
     }
 
     public void pluck() {
-
+    	for (int x = 0; x< capacity; x++) {
+        	buffer.dequeue();
+        }
+    	for (int x = 0; x< capacity; x++) {
+        	buffer.enqueue(Math.random() - .5);
+        }
     }
 
     void tic() {
-
+    	time++;
     }
 
     double sample() {
-
+    	return buffer.peek();
     }
 
     int time() {
-
+    	return time;
     }
 }
