9e387b7adc9cf20132384cd075de62da73e2bf35
SeasideWhisperer <benjaminhouseholder0@gmail.com>
2024-11-01 13:42:25 -0500

    pushin

--- src/GuitarHero.java
+++ src/GuitarHero.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*****************************************************************************
  *  Compilation:  javac GuitarHeroLite.java
  *  Execution:    java  GuitarHeroLite
@@ -12,23 +15,35 @@
 public class GuitarHero {
 
     public static void main(String[] args) {
-
+    	 System.out.println(getNote(1));
         // Create two guitar strings, for concert A and C
-        double CONCERT_A = 440.0;
-        double CONCERT_C = CONCERT_A * Math.pow(2, 3.0/12.0);
-        GuitarString stringA = new GuitarString(CONCERT_A);
-        GuitarString stringC = new GuitarString(CONCERT_C);
-
-        final double TEXT_POS_X = .2;
+    	 
+    	 GuitarString[] strings = new GuitarString[37];
+    	    for (int x = 0; x<37; x++) {
+    	    	strings[x] = new GuitarString(getNote(x+1));
+    	   }
+        
+        
+        final double TEXT_POS_X = .4;
         final double TEXT_POS_Y = .5;
-        
-        StdDraw.text(TEXT_POS_X, TEXT_POS_Y, "Type 'a' or 'c' to play a note!");
-        
-        play(stringA, stringC);
+        final double TEXT_POS_Y2 = .4;
+        StdDraw.text(TEXT_POS_X, TEXT_POS_Y, "Type Notes in the Following Format (S=Space):");
+        StdDraw.text(TEXT_POS_X+.02, TEXT_POS_Y -.1, "2  4 5  7 8 9  - =");
+        StdDraw.text(TEXT_POS_X, TEXT_POS_Y -.15, "q we r ty u i op [");
+        StdDraw.text(TEXT_POS_X, TEXT_POS_Y2 -.1, "  d f g    j k  ; ,");
+        StdDraw.text(TEXT_POS_X, TEXT_POS_Y2 -.15, "zx c v bn m ,. / S");
+        play(strings);
+       
     }
     
-    private static void play(GuitarString stringA, GuitarString stringC) {        // the main input loop
-        while (true) {
+    public static double getNote(int i) {
+    	return 440* Math.pow(1.05956, i-25);
+    }
+    
+    
+    private static void play(GuitarString[] strings) {        // the main input loop
+    	String bigString = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    	while (true) {
             
             // check if the user has typed a key, and, if so, process it
             if (StdDraw.hasNextKeyTyped()) {
@@ -37,21 +52,22 @@ public class GuitarHero {
                 char key = StdDraw.nextKeyTyped();
 
                 // pluck the corresponding string
-                if (key == 'a') 
-                    stringA.pluck();
-                else if (key == 'c') 
-                    stringC.pluck();
+                int noteNum = bigString.indexOf(key);
+                if (noteNum != -1) 
+                	strings[noteNum].pluck();
             }
 
             // compute the superposition of the samples
-            double sample = stringA.sample() + stringC.sample();
+            double sample = 0.0;
+            for (int x = 0; x< strings.length; x++) {
+            	sample += strings[x].sample();
+            }
 
             // send the result to standard audio
             StdAudio.play(sample);
-
-            // advance the simulation of each guitar string by one step
-            stringA.tic();
-            stringC.tic();
+            for (int x = 0; x< strings.length; x++) {
+            	strings[x].tic();
+            }
         }
         
     }
